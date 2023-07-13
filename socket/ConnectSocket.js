8b30befc0dac1c4961d8cfc063674672eb93d1af
Shalu Ambasta <itsambastakashish@gmail.com>
2023-07-13 17:31:53 +0530

    socket connected 😁

--- socket/ConnectSocket.js
+++ socket/ConnectSocket.js
@@ -1,3 +1,17 @@
-function connectSocket() {}
+const socketIo = require("socket.io");
+
+function connectSocket(server) {
+  io = new socketIo.Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A client to socket connected having ID:", socket.id);
+  });
+}
 
 module.exports = { connectSocket };
