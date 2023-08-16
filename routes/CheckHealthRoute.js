ad0af4a87e8e037d015688cb5017768930ab7544
Shalu Ambasta <itsambastakashish@gmail.com>
2023-08-16 09:41:29 +0530

    routes updated 🤓

--- routes/CheckHealthRoute.js
+++ routes/CheckHealthRoute.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/health", (req, res) => {
-  res.send(`Server is healthy 💚`);
+  res.send(`Server is healthy 💚 `);
 });
 
 module.exports = router;
